3ca065e0e82c2996147b7b515dd4a223a25b1463
Shalom Abrokwah <shalomabrokwah@gmail.com>
2023-02-10 10:25:58 +0100

    Set up the animations controllers

--- lib/views/stacked_animation.dart
+++ lib/views/stacked_animation.dart
@@ -1,3 +1,5 @@
+import 'dart:math';
+
 import 'package:flutter/material.dart';
 
 class StackedAnimation extends StatefulWidget {
@@ -7,7 +9,37 @@ class StackedAnimation extends StatefulWidget {
   State<StackedAnimation> createState() => _StackedAnimationState();
 }
 
-class _StackedAnimationState extends State<StackedAnimation> {
+class _StackedAnimationState extends State<StackedAnimation>
+    with TickerProviderStateMixin {
+  late AnimationController xController;
+  late AnimationController yController;
+  late AnimationController zController;
+
+  late Tween<double> animator;
+
+  @override
+  void initState() {
+    super.initState();
+    xController =
+        AnimationController(vsync: this, duration: const Duration(seconds: 20));
+
+    yController =
+        AnimationController(vsync: this, duration: const Duration(seconds: 20));
+
+    zController =
+        AnimationController(vsync: this, duration: const Duration(seconds: 20));
+
+    animator = Tween(begin: 0, end: pi / 2);
+  }
+
+  @override
+  void dispose() {
+    zController.dispose();
+    yController.dispose();
+    xController.dispose();
+    super.dispose();
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -18,7 +50,16 @@ class _StackedAnimationState extends State<StackedAnimation> {
       body: Padding(
         padding: const EdgeInsets.all(16),
         child: SafeArea(
-          child: Column(),
+          child: Column(
+            children: [
+              const SizedBox(
+                height: 100,
+              ),
+              Stack(
+                children: const [],
+              )
+            ],
+          ),
         ),
       ),
     );
